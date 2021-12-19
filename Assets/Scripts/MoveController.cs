d03ff440a68ab60d4d224cd53c81be9ed0d4e5e4
jose eduardo <joseleon1903@outlook.com>
2021-12-19 12:26:05 -0400

    commit setup project

---
+++ Assets/Scripts/MoveController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class MoveController : MonoBehaviour
+{
+
+    /// <summary>
+    /// 
+    ///     Move the gameObjec trasform on direction Vector2
+    /// </summary>
+    /// <param name="direction"></param>
+    public void move(Vector2 direction) { 
+        transform.position += transform.forward * direction.x * Time.deltaTime +
+            transform.right * direction.y * Time.deltaTime;
+    }
+
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(MoveController))]
+public class Player : MonoBehaviour
+{
+
+    [System.Serializable]
+    public class MouseInput {
+        public Vector2 Damping;
+        public Vector2 Sensitivity;
+    }
+
+    [SerializeField]
+    private float speed;
+
+    [SerializeField]
+    private MouseInput MouseControl;
+
+    // variable of the object
+    private MoveController m_moveController;
+
+    public MoveController MoveController { 
+        get 
+        {
+            if (m_moveController == null)
+                m_moveController = GetComponent<MoveController>();
+            return m_moveController; 
+        }  
+    }
+
+    private InputController playeInput;
+
+    Vector2 mouseInput;
+
+    void Awake() {
+        playeInput = GameManager.Instance.InputController;
+        GameManager.Instance.LocalPlayer = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 direction = new Vector2(playeInput.Vertical * speed, playeInput.Horizontal * speed);
+        MoveController.move(direction);
+
+        mouseInput.x = Mathf.Lerp(mouseInput.x, playeInput.MouseInput.x, 1f / MouseControl.Damping.x);
+
+        transform.Rotate(Vector3.up * mouseInput.x * MouseControl.Sensitivity.x);
+    }
+}
