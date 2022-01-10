58ac87b5f28428e936dc716219ee4eccb0198797
jose eduardo <joseleon1903@outlook.com>
2022-01-10 13:30:55 -0400

    add ui module and tween module

---
+++ Assets/Scripts/WebSocket/ServerCommunication.cs
@@ -0,0 +1,74 @@
+﻿using Assets.Script.WebSocket;
+using Assets.Scripts.WebSocket;
+using UnityEngine;
+
+[RequireComponent(typeof(SocketConfig))]
+public class ServerCommunication : MonoBehaviour
+{
+
+    // Final server address
+    private SocketConfig serverConfig;
+
+    private string server;
+
+    // WebSocket Client 
+    private WsClient client;
+
+    /// <summary>
+    /// Unity method called on initialization
+    /// </summary>
+    private void Awake()
+    {
+        serverConfig = GetComponent<SocketConfig>();
+        server = serverConfig.GetSocketHost();
+        client = new WsClient(server);
+    }
+
+    /// <summary>
+    /// Unity method called every frame
+    /// </summary>
+    private void Update()
+    {
+        // Check if server send new messages
+        var cqueue = client.receiveQueue;
+        string msg;
+        while (cqueue.TryPeek(out msg))
+        {
+            // Parse newly received messages
+            cqueue.TryDequeue(out msg);
+            HandleMessage(msg);
+        }
+    }
+
+    /// <summary>
+    /// Method responsible for handling server messages
+    /// </summary>
+    /// <param name="msg">Message.</param>
+    private void HandleMessage(string msg)
+    {
+        Debug.Log("Server: " + msg);
+    }
+
+
+    /// <summary>
+    /// Call this method to connect to the server
+    /// </summary>
+    public async void ConnectToServer()
+    {
+        await client.Connect();
+    }
+
+    /// <summary>
+    /// Method which sends data through websocket
+    /// </summary>
+    /// <param name="message">Message.</param>
+    public void SendRequest(string message)
+    {
+        client.Send(message);
+    }
+
+    public async void DisconectToServer()
+    {
+        await client.Disconect();
+    }
+}
---
+++ Assets/Scripts/WebSocket/SocketConfig.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace Assets.Script.WebSocket
+{
+    class SocketConfig : MonoBehaviour
+    {
+        // Server IP address
+        [SerializeField]
+        private string hostIP;
+
+        // Server port
+        [SerializeField]
+        private int port = 8080;
+
+        // Flag to use localhost
+        [SerializeField]
+        private bool useLocalhost = true;
+
+        [SerializeField]
+        private string remoteHost;
+
+        public string GetSocketHost()
+        {
+
+            string url = "ws://";
+
+            if (useLocalhost)
+            {
+
+                return $"ws://91933459b031.ngrok.io/lobby-manager";
+
+            }
+
+            return url + remoteHost + "/lobby-manager";
+
+        }
+    }
+}
